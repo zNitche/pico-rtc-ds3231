807106e8387385b7bd9d6d6f8fa7ff66e4ef3d85
zNitche <53448802+zNitche@users.noreply.github.com>
2023-05-24 16:37:21 +0200

    driver for ds3231

---
+++ ds3231.py
@@ -0,0 +1,74 @@
+# Micropython driver for the DS3231 RTC Module
+# Lightweight version of https://github.com/pangopi/micropython-DS3231-AT24C32/blob/main/ds3231.py
+
+from micropython import const
+
+
+DATETIME_REG = const(0)
+STATUS_REG = const(15)
+
+
+class DS3231:
+    def __init__(self, i2c, addr=0x68):
+        self.i2c = i2c
+        self.addr = addr
+        self._time_buffer = bytearray(7)
+
+    def is_device_accessible(self):
+        return True if self.addr in self.i2c.scan() else False
+
+    def dec2bcd(self, decimal):
+        return (decimal // 10) << 4 | (decimal % 10)
+
+    def bcd2dec(self, bcd):
+        return ((bcd >> 4) * 10) + (bcd & 0x0F)
+
+    def get_date(self, check_accuracy=False):
+        if check_accuracy and self.osf():
+            raise Exception("Oscillator stop flag set. Time may not be accurate.")
+
+        self.i2c.readfrom_mem_into(self.addr, DATETIME_REG, self._time_buffer)
+
+        seconds = self.bcd2dec(self._time_buffer[0])
+        minutes = self.bcd2dec(self._time_buffer[1])
+        hour = self.bcd2dec(self._time_buffer[2])
+
+        day = self.bcd2dec(self._time_buffer[4])
+        month = self.bcd2dec(self._time_buffer[5] & 0x7f)
+        year = self.bcd2dec(self._time_buffer[6]) + 2000
+
+        return Date(year, month, day, hour, minutes, seconds)
+
+    def set_date(self, date):
+        self._time_buffer[0] = self.dec2bcd(date.seconds)
+        self._time_buffer[1] = self.dec2bcd(date.minutes)
+        self._time_buffer[2] = self.dec2bcd(date.hour)
+        self._time_buffer[4] = self.dec2bcd(date.day)
+        self._time_buffer[5] = self.dec2bcd(date.month) & 0xff
+        self._time_buffer[6] = self.dec2bcd(int(str(date.year)[-2:]))
+
+        self.i2c.writeto_mem(self.addr, DATETIME_REG, self._time_buffer)
+
+        self.reset_osf()
+
+    def osf(self):
+        return bool(self.i2c.readfrom_mem(self.addr, STATUS_REG, 1)[0] >> 7)
+
+    def reset_osf(self):
+        reg_buffer = bytearray(1)
+
+        self.i2c.readfrom_mem_into(self.addr, STATUS_REG, reg_buffer)
+        self.i2c.writeto_mem(self.addr, STATUS_REG, bytearray([reg_buffer[0] & 0x7f]))
+
+
+class Date:
+    def __init__(self, year, month, day, hour, minutes, seconds):
+        self.seconds = seconds
+        self.minutes = minutes
+        self.hour = hour
+        self.day = day
+        self.month = month
+        self.year = year
+
+    def datetime(self):
+        return (self.year, self.month, self.day, self.hour, self.minutes, self.seconds)
---
+++ main.py
@@ -0,0 +1,16 @@
+import machine
+import ds3231
+
+
+def main():
+    i2c = machine.I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
+    rtc_module = ds3231.DS3231(i2c)
+
+    if rtc_module.is_device_accessible():
+        # rtc_module.set_date(ds3231.Date(2023, 5, 24, 17, 10, 0))
+
+        print(rtc_module.get_date(check_accuracy=True).datetime())
+
+
+if __name__ == '__main__':
+    main()
